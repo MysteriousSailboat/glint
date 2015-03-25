4bcfffd05e5dd8632e458cd14b655a9f6c0d392e
ChadFong <ChadMFong@Gmail.com>
2015-03-25 16:05:52 -0700

    (refactor) database skeleton to board verbage
    Copied ideas database files have been refactored to using 'board'
      in lieu of 'idea'.  Awaiting implementation of front-end and
      routes to determine if functional.

--- server/boards/boardController.js
+++ server/boards/boardController.js
@@ -1,45 +1,45 @@
-// Idea Controller
+// Board Controller
 // ---------------
 //
 // The idea controller handles requests passed from the idea router.
 
 // The Q module is used to bind Mongoose methods to use promises.
 var Q = require('q');
-var Idea = require('./ideaModel.js');
+var Board = require('./boardModel.js');
 
 module.exports = {
 
-  // Retrieve all of the ideas that exist in the MongoDB database.
-  allIdeas: function(req, res, next) {
+  // Retrieve all of the boards that exist in the MongoDB database.
+  allBoards: function(req, res, next) {
     
     // Bind the Mongoose find method to the Idea model, so that the Q module can use promises with it.
-    var findAllIdeas = Q.nbind(Idea.find, Idea);
+    var findAllBoards = Q.nbind(Board.find, Board);
     
-    findAllIdeas({})
-      .then(function(ideas) {
-        res.json(ideas);
+    findAllBoards({})
+      .then(function(boards) {
+        res.json(boards);
       })
       .fail(function(error) {
         next(error);
       });
   },
 
-  // Add a new idea to the MongoDB database.
-  newIdea: function(req, res, next) {
+  // Add a new Board to the MongoDB database.
+  newBoard: function(req, res, next) {
 
-    // Bind the Mongoose create method to the Idea model, so that the Q module can use promises with it.
-    var createIdea = Q.nbind(Idea.create, Idea);
+    // Bind the Mongoose create method to the Board model, so that the Q module can use promises with it.
+    var createBoard = Q.nbind(Board.create, Board);
 
-    // Create a new document from the Idea model. If successfully created then the new Idea document is returned.
-    var newIdea = {
+    // Create a new document from the Board model. If successfully created then the new Board document is returned.
+    var newBoard = {
       title: req.body.title,
       text: req.body.text
     };
 
-    createIdea(newIdea)
-      .then(function (createdIdea) {
-        if (createdIdea) {
-            res.json(createdIdea);
+    createBoard(newBoard)
+      .then(function (createdBoard) {
+        if (createdBoard) {
+            res.json(createdBoard);
         }
       })
       .fail(function(error) {
