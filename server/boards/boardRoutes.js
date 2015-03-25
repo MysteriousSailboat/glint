dbc92819f0cb328f178bb3e64adef0157ac42000
ChadFong <ChadMFong@Gmail.com>
2015-03-25 16:05:52 -0700

    (refactor) database skeleton to board verbage
    Copied ideas database files have been refactored to using 'board'
      in lieu of 'idea'.  Awaiting implementation of front-end and
      routes to determine if functional.

--- server/boards/boardRoutes.js
+++ server/boards/boardRoutes.js
@@ -1,13 +1,13 @@
-// Idea Routes
+// Board Routes
 // -----------
 //
-// The Idea routes further routes any requests to /api/ideas in the middleware to specific Idea methods defined in the Idea controller.
+// The Board routes further routes any requests to /api/ideas in the middleware to specific Idea methods defined in the Idea controller.
 
-var ideaController = require('./ideaController.js');
+var boardController = require('./boardController.js');
 
 module.exports = function (app) {
-    // Further route from the /api/ideas path. A GET will return all of the posted ideas from the database. A POST will add a new idea to the database.
+    // Further route from the /api/boards path. A GET will return all of the posted boards from the database. A POST will add a new board to the database.
     app.route('/')
-      .get(ideaController.allIdeas)
-      .post(ideaController.newIdea);
+      .get(boardController.allBoards)
+      .post(boardController.newBoard);
 };
