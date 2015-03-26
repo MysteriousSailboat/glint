cd5c5d59577e059a8630c788e16fe31837964b25
ChadFong <ChadMFong@Gmail.com>
2015-03-25 17:46:02 -0700

    (refactor) front-end to run boards instead of ideas
    In order to have ideas hierarchically under boards, boards must
    show first.  This refactors the board files copied from the ideas
    files to reflect the back-end that has been created for boards.
    
    Currently saving boards to database.  Does not display them

--- client/app/boards/boards.js
+++ client/app/boards/boards.js
@@ -1,30 +1,30 @@
-// Ideas controller
+// Boards controller
 // ----------------
 //
 
-// The pattern we're using here is the pattern we're using across all our controllers: the controllerAs syntax. This syntax is for Angular versions 1.2 and up, and means you don't have to use `$scope` anymore. Instead, inside of your HTML, you declare your controller with `ng-controller="IdeasCtrl as ictrl"` and reference your variables within that controlled scope as `ictrl.<varname>`. Additionally, instead of setting your properties within your controller to `$scope`, assign your controller's `this` to a variable called self and set your properties to that. 
-angular.module('glint.ideas', [])
-.controller('IdeasCtrl', function (Ideas, $filter){
+// The pattern we're using here is the pattern we're using across all our controllers: the controllerAs syntax. This syntax is for Angular versions 1.2 and up, and means you don't have to use `$scope` anymore. Instead, inside of your HTML, you declare your controller with `ng-controller="BoardsCtrl as ictrl"` and reference your variables within that controlled scope as `ictrl.<varname>`. Additionally, instead of setting your properties within your controller to `$scope`, assign your controller's `this` to a variable called self and set your properties to that. 
+angular.module('glint.boards', [])
+.controller('BoardsCtrl', function (Boards, $filter){
   var self = this;
-  self.data = { ideas: [] };
-  self.idea = {};
+  self.data = { boards: [] };
+  self.board = {};
   self.postSuccess = false;
   self.submitted = false;
 
-  // Display all ideas currently in the database.
-  self.displayIdeas = function(){
-    Ideas.getIdeas()
+  // Display all boards currently in the database.
+  self.displayBoards = function(){
+    Boards.getBoards()
       .then(function (results){
         results = $filter('orderBy')(results, 'votes', true);
-        self.data.ideas = results;
+        self.data.boards = results;
       })
       .catch(function (error){
-        console.error('displayIdeas error', error);
+        console.error('displayBoards error', error);
       });
   };
 
-  // Submit a new idea.
-  self.submitIdea = function ($timeout){
+  // Submit a new board.
+  self.submitBoard = function ($timeout){
 
     // Show description box.
     if (self.submitted === false){
@@ -32,26 +32,26 @@ angular.module('glint.ideas', [])
     } else {
 
     // Escape user input.
-    self.idea.title = _.escape(self.idea.title);
-    self.idea.text = _.escape(self.idea.text);
-    var idea = JSON.stringify(self.idea);
+    self.board.boardName = _.escape(self.board.boardName);
+    self.board.caption = _.escape(self.board.caption);
+    var board = JSON.stringify(self.board);
     
-    // POST new idea, display confirmation, redisplay all ideas.
-    Ideas.createIdea(idea)
+    // POST new board, display confirmation, redisplay all boards.
+    Boards.createBoard(board)
       .then(function (response){
         // Show user feedback.
         self.postSuccess = true;
         // Hide idea description field.
         self.submitted = false;
         // Clear form fields after submit.
-        self.idea = {};
-        self.displayIdeas();
+        self.board = {};
+        self.displayBoards();
       })
       .catch(function (error){
-        console.error('createIdea error', error);
+        console.error('createBoard error', error);
       });
     }
   };
 
-  self.displayIdeas();
+  self.displayBoards();
 });
