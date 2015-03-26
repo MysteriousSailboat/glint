4008c129fccc06a43a2ab74c901339765e106e6a
david-hammer <dhammer126@gmail.com>
2015-03-25 17:16:57 -0700

    begin implementing user auth

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -2,3 +2,48 @@
 // ---------------
 //
 // The User controller handles requests passed from the User router.
+
+var Q = require('q');
+var User = require('./userModel.js');
+
+module.exports = {
+
+  // Retrieve all of the ideas that exist in the MongoDB database.
+  login: function(req, res, next) {
+    
+    // Bind the Mongoose find method to the Idea model, so that the Q module can use promises with it.
+    var findAllIdeas = Q.nbind(Idea.find, Idea);
+    
+    findAllIdeas({})
+      .then(function(ideas) {
+        res.json(ideas);
+      })
+      .fail(function(error) {
+        next(error);
+      });
+  },
+
+  // Add a new idea to the MongoDB database.
+  signup: function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var createUser = Q.nbind(User.create, User);
+
+    var newUser = {
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    createUser(newUser)
+      .then(function (createdUser) {
+        if (createdIdea) {
+          res.json(createdIdea);
+        }
+      })
+      .fail(function(error) {
+        next(error);
+      });
+
+  }
+};
--- server/users/usersModel.js
+++ server/users/usersModel.js
@@ -4,6 +4,8 @@
 // The User model defines the structure of all of the User documents created.
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var bluebird = require('bluebird');
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -18,4 +20,27 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('users', UserSchema);
+var User = mongoose.model('users', UserSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch){
+    if (err) {
+      console.log('login error');
+      console.error(err);
+    } else {
+      cb(null, isMatch);
+    }
+  });
+};
+
+UserSchema.pre('save', function(next){
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash){
+      this.password = hash;
+      next();
+    });
+});
+
+
+module.exports = User;
