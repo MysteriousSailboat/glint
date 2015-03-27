9736926e21f5f74e80ec22415797e05400c8bb9a
Michael Roberts <m_roberts6@u.pacific.edu>
2015-03-27 15:39:29 -0700

    (refactor) reimplement without using  in authControl

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -9,9 +9,9 @@ angular.module('glint.auth', [])
   var self = this;
   self.user = {};
   // for displaying the login error message
-  $scope.loginUsernameFailure = false;
-  $scope.loginPasswordFailure = false;
-  $scope.userNameExists = false;
+  self.loginUsernameFailure = false;
+  self.loginPasswordFailure = false;
+  self.userNameExists = false;
 
   // Allow user to declare who they are to the system.
   self.signin = function() {
@@ -21,9 +21,8 @@ angular.module('glint.auth', [])
 
     Auth.signin(user)
     .then(function (res){
-      $scope.loginUsernameFailure = false;
-      $scope.loginPasswordFailure = false;
-      $scope.loginFailure = false;
+      self.loginUsernameFailure = false;
+      self.loginPasswordFailure = false;
       token = res.data;
       $window.localStorage.setItem('com.glint', JSON.stringify(token));
       $location.path('/');
@@ -31,16 +30,16 @@ angular.module('glint.auth', [])
     .catch(function (error){
       //render error happens in Auth.signin service
       if (error.status === 401 && error.status === 404) {
-        $scope.loginUsernameFailure = true;
-        $scope.loginPasswordFailure = true;
+        self.loginUsernameFailure = true;
+        self.loginPasswordFailure = true;
       }
       else if (error.status === 401) {
-        $scope.loginUsernameFailure = false;
-        $scope.loginPasswordFailure = true;
+        self.loginUsernameFailure = false;
+        self.loginPasswordFailure = true;
       }
       else if (error.status === 404) {
-        $scope.loginPasswordFailure = false;
-        $scope.loginUsernameFailure = true;
+        self.loginPasswordFailure = false;
+        self.loginUsernameFailure = true;
       }
     });
   };
@@ -53,14 +52,14 @@ angular.module('glint.auth', [])
 
     Auth.signup(user)
     .then(function (res){
-      $scope.userNameExists = false;
+      self.userNameExists = false;
       token = res.data;
       $window.localStorage.setItem('com.glint', JSON.stringify(token));
       $location.path('/');
     })
     .catch(function (error){
       console.error('signup error', error);
-      $scope.userNameExists = true;
+      self.userNameExists = true;
     });
   };
 
